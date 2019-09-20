3aa8d8491d38c6bcda88d6de3a3b295c3f6a3b33
Michael Wali <michael.wali@andela.com>
2019-09-20 09:14:39 +0300

    add tests for product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+    description: "yyy", image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 1"],product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 1"],product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
 end
