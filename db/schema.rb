f003fb45b89d2bf7f3b86bc34e9492e5fdd9ee8a
Michael Wali <michael.wali@andela.com>
2019-09-21 17:02:27 +0300

    add cart creation process for app

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190919151334) do
+ActiveRecord::Schema.define(version: 20190921132014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+    t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -24,4 +38,6 @@ ActiveRecord::Schema.define(version: 20190919151334) do
     t.datetime "updated_at",                          null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
