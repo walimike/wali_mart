73e48d9b84fdf62ef2727268c246025435ba8616
Michael Wali <michael.wali@andela.com>
2019-09-22 16:56:08 +0300

    modify schema.rb file

--- db/migrate/20190922134702_combine_items_in_cart.rb
+++ db/migrate/20190922134702_combine_items_in_cart.rb
@@ -1,6 +1,5 @@
 class CombineItemsInCart < ActiveRecord::Migration[5.0]
   def up
-    # replace multiple items for a single product in a cart with a # single item
     Cart.all.each do |cart|
         # count the number of each product in the cart
       sums = cart.line_items.group(:product_id).sum(:quantity)
@@ -9,7 +8,8 @@ class CombineItemsInCart < ActiveRecord::Migration[5.0]
               # remove individual items
           cart.line_items.where(product_id: product_id).delete_all
                   # replace with a single item
-          item = cart.line_items.build(product_id: product_id) item.quantity = quantity
+          item = cart.line_items.build(product_id: product_id)
+          item.quantity = quantity
           item.save!
         end
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190922133845) do
+ActiveRecord::Schema.define(version: 20190922134702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
