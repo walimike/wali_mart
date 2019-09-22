da6cc543c14481281e651058dde0db2454f619b2
Michael Wali <michael.wali@andela.com>
2019-09-22 16:40:57 +0300

    modify db schema to cater for quantity integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190921132014) do
+ActiveRecord::Schema.define(version: 20190922133845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20190921132014) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
     t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
   end
