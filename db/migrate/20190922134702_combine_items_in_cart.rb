4d0557468df271ca729fb690e47b5ac2af5a479a
Michael Wali <michael.wali@andela.com>
2019-09-24 08:39:29 +0300

    add migration rollback code

--- db/migrate/20190922134702_combine_items_in_cart.rb
+++ db/migrate/20190922134702_combine_items_in_cart.rb
@@ -15,4 +15,16 @@ class CombineItemsInCart < ActiveRecord::Migration[5.0]
       end
     end
   end
+
+  def down
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id, product_id: line_item.product_id, quantity: 1
+        )
+      end
+        # remove original item
+      line_item.destroy
+    end
+  end
 end
